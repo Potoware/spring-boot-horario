66350543abc7123554de517ad6756ad7658f65be
Potoware <alejandropotosi@outlook.com>
2021-09-14 19:49:54 -0500

    583. Registrando interceptor

--- src/main/java/com/potoware/springboot/horario/interceptors/HorarioInterceptor.java
+++ src/main/java/com/potoware/springboot/horario/interceptors/HorarioInterceptor.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component
+@Component("horarioInterceptor")
 public class HorarioInterceptor implements HandlerInterceptor {
 	@Value("${config.horario.apertura}")
 	private Integer apertura;
